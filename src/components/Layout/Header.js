8baa1d522e2080588232f34736b9bf56384b73ae
nawedrashid <75127419+nawedrashid@users.noreply.github.com>
2022-08-05 20:51:46 +0530

    Update Header.js

--- src/components/Layout/Header.js
+++ src/components/Layout/Header.js
@@ -6,7 +6,7 @@ import HeaderCartButton from './HeaderCartButton';
 const Header = props => {
     return <Fragment>
         <header className={classes.header}>
-            <h1>ReactMeals</h1>
+            <h1>Meals</h1>
             <HeaderCartButton onClick={props.onShowCart}/> 
         </header>
         <div className={classes['main-image']}>
